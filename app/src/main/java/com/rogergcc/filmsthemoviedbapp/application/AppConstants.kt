d129b495cce2bfff22318045bcd51ac1e919c8ce
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2021-09-20 17:13:56 -0500

    update addin key for debugg hilt

--- app/src/main/java/com/rogergcc/filmsthemoviedbapp/application/AppConstants.kt
+++ app/src/main/java/com/rogergcc/filmsthemoviedbapp/application/AppConstants.kt
@@ -7,5 +7,5 @@ object AppConstants {
     //API
     //https://api.themoviedb.org/3/movie/popular?api_key=09963e300150f9831c46a1828a82a984&language=en-US
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY = BuildConfig.API_KEY
+    const val API_KEY = "09963e300150f9831c46a1828a82a984"
 }
