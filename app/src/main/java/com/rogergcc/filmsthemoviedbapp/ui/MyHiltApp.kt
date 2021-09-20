2485942aefffadb504417063a99cfd8d9eaf5fc1
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2021-09-19 20:38:15 -0500

    adding DI dagger hilt to graddle

---
+++ app/src/main/java/com/rogergcc/filmsthemoviedbapp/ui/MyHiltApp.kt
@@ -0,0 +1,8 @@
+package com.rogergcc.filmsthemoviedbapp.ui
+
+import android.app.Application
+//import dagger.hilt.android.HiltAndroidApp
+
+//@HiltAndroidApp
+class MyHiltApp: Application() {
+}
---
+++ app/src/main/java/com/rogergcc/filmsthemoviedbapp/ui/utils/ViewUtils.kt
@@ -0,0 +1,13 @@
+package com.rogergcc.filmsthemoviedbapp.ui.utils
+
+import android.view.View
+
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
