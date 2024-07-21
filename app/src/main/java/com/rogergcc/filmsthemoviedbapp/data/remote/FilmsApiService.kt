5844e6ee116c2956bbe0a722c51c7f59eb7ffc12
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2024-07-20 23:24:04 -0500

    wip change from popular to ListCollection

--- app/src/main/java/com/rogergcc/filmsthemoviedbapp/data/remote/FilmsApiService.kt
+++ app/src/main/java/com/rogergcc/filmsthemoviedbapp/data/remote/FilmsApiService.kt
@@ -1,5 +1,6 @@
 package com.rogergcc.filmsthemoviedbapp.data.remote
 
+import com.rogergcc.filmsthemoviedbapp.data.remote.model.CollectionMoviesResponse
 import com.rogergcc.filmsthemoviedbapp.data.remote.model.MovieListResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,12 +9,19 @@ import java.io.IOException
 interface FilmsApiService {
     @GET("movie/popular")
     suspend fun getPopularMovies(@Query("api_key") apiKey: String): MovieListResponse
+
+    @GET("list/15570")
+    suspend fun getListMovies(@Query("api_key") apiKey: String, @Query("page") page: Int ): CollectionMoviesResponse
 }
 
 class FakeFilmsApiService : FilmsApiService {
     // Simula una excepción de red
     override suspend fun getPopularMovies(apiKey: String): MovieListResponse {
         throw IOException("Network error occurred")
+    }
+
+    override suspend fun getListMovies(apiKey: String, page: Int): CollectionMoviesResponse {
+        TODO("Not yet implemented")
     }
 }
 
